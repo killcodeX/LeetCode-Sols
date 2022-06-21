6796647b21e4267c363f1fdd6f2abcdb651cb62d
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2022-06-21 09:45:45 +0530

    sol added

---
+++ Binary Trees/Root Equals Sum of Children (2236)/sol.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+ var checkTree = function(root) {
+    
+    if(root.val == (root.left.val + root.right.val)){
+        return true
+    }
+    
+    return false
+};
