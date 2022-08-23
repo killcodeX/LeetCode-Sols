e7dca52a6096a0fc17774878bcf9013c249b911a
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2022-08-23 12:07:32 +0530

    added new pattern

--- Sliding Window/Number of Substrings Containing All Three Characters (1358)/sol.js
+++ Sliding Window/Number of Substrings Containing All Three Characters (1358)/sol.js
@@ -11,11 +11,14 @@ var numberOfSubstrings = function (s) {
   while (end < s.length) {
     count[s[end].charCodeAt() - 97]++;
     while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
+      console.log("inside while --", { start, end, count });
       count[s[start].charCodeAt() - 97]--;
       start++;
     }
+    console.log("after while --", { start, end, count });
     occ += start;
     end++;
+    console.log("---------------------------------------------------------");
   }
 
   return occ;
