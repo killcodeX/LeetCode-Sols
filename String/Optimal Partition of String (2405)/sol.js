bf1a32af7a8dba01b612e6438eebfcb1776583ec
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2022-09-11 15:46:45 +0530

    sol added

---
+++ String/Optimal Partition of String (2405)/sol.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var partitionString = function (s) {
+  let nSub = 1;
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (!stack.includes(s[i])) {
+      stack.push(s[i]);
+    } else {
+      nSub++;
+      stack = [];
+      stack.push(s[i]);
+    }
+  }
+
+  return nSub;
+};
