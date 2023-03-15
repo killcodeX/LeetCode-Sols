375fbc76927a791682ef065bafa9d8b7ca316ab5
Aaquib Ahmed <43428865+killcodeX@users.noreply.github.com>
2023-03-15 23:06:56 +0530

    sol added

---
+++ Binary Search/Single Element in a Sorted Array (540)/sol.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNonDuplicate = function (nums) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.trunc(right - left / 2);
+
+    if (nums[mid - 1] != nums[mid] && nums[mid] != nums[mid + 1]) {
+      return nums[mid];
+    }
+
+    if (mid % 2 == 0 && nums[mid] == nums[mid + 1]) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+};
